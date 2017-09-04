8c2eb4e31dbb04d596e7c04a4dcc2d3067ae3ff6
Kayla Hansen <shiverr@gmail.com>
2017-09-04 18:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === lowercase) {
     return `I can\t hear you`
-  } else if (string === uppercase) {
+  } else {
     return `YES INDEED!`
   }
   }
